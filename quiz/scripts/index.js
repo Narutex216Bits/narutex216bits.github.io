bf8ddaf9348984fd1f0bab31b40768722e5c0fa9
Marcio Henrique de Tulio <marcio.tulio@etec.sp.gov.br>
2024-08-09 22:01:30 -0300

    Delete quiz directory
    delete quiz

--- quiz/scripts/index.js
+++
@@ -1,8 +0,0 @@
-function definirMateria(materiaEscolhida) {
-    sessionStorage.setItem('materia', materiaEscolhida);
-    window.location.href = '/pages/pergunta.html'
-}
-
-function definirMateriaComTagLink(materiaEscolhida) {
-    sessionStorage.setItem('materia', materiaEscolhida);
-}
